799b659d3caa4de5ce1ea733652d97adeb4a3e30
둘리 <parrot0707@gmail.com>
2023-01-08 18:50:24 +0900

    하단바 mui 적용

--- src/BottomNav.tsx
+++ src/BottomNav.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import './BottomNav.css';
 import { Link } from 'react-router-dom';
+import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import SettingsIcon from '@mui/icons-material/Settings';
+import CreateIcon from '@mui/icons-material/Create';
 
 const BottomNav = () => {
+  const [value, setValue] = React.useState(0);
+
   return (
-    <nav className="wrapper">
-      <div>
-        <Link to="/record" className="nav-link">
-          기록
-        </Link>
-      </div>
-      <div>
-        <Link to="/statistics" className="nav-link">
-          통계
-        </Link>
-      </div>
-      <div>
-        <Link to="/settings" className="nav-link">
-          설정
-        </Link>
-      </div>
-    </nav>
+    <BottomNavigation
+      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+      showLabels
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+    >
+      <BottomNavigationAction
+        label="기록"
+        icon={<CreateIcon />}
+        component={Link}
+        to="/record"
+      />
+      <BottomNavigationAction
+        label="통계"
+        icon={<BarChartIcon />}
+        component={Link}
+        to="/statistics"
+      />
+      <BottomNavigationAction
+        label="설정"
+        icon={<SettingsIcon />}
+        component={Link}
+        to="/settings"
+      />
+    </BottomNavigation>
   );
 };
 
